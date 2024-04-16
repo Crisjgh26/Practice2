cc729a78b6dc650e6357007c10225d515bd209b3
Murphy Horta Camargo <murphy@heippi.com>
2024-04-16 15:18:20 -0500

    api inicial

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express")
 
 const app = express();
 
-
-
+//Default Endpoint
 app.get("/", (req, rest) => { rest.send("hello world") })
 
 app.listen(3000, () => {
-    console.log(" api inicializada")
+    console.log("api inicializada")
 });
